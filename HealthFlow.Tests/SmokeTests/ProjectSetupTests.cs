1717ff7c36b855c1044d464d123c5c27fa4226a7
gabriela0402 <gabiromano2018@gmail.com>
2026-09-25 19:58:23 -0300

    test: adiciona projeto de testes unitarios

---
+++ HealthFlow.Tests/SmokeTests/ProjectSetupTests.cs
@@ -0,0 +1,17 @@
+﻿namespace HealthFlow.Tests.SmokeTests;
+
+public class ProjectSetupTests
+{
+    [Fact]
+    public void Project_Should_Be_Configured_Correctly()
+    {
+        // Arrange
+        const bool projectIsConfigured = true;
+
+        // Act
+        var result = projectIsConfigured;
+
+        // Assert
+        Assert.True(result);
+    }
+}
